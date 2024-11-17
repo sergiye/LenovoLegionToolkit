1d140858de910d6dd5fb145c9cc9e68c1ad0967b
Sergiy Egoshyn <egoshin.sergey@gmail.com>
2024-11-17 11:16:43 +0200

    build script fix + github actions enabled

--- make.bat
+++ make.bat
@@ -7,10 +7,12 @@ IF "%1"=="" (
 )
 	
 
-SET PATH=%PATH%;"C:\Program Files (x86)\Inno Setup 6"
+rem SET PATH=%PATH%;"C:\Program Files (x86)\Inno Setup 6"
 
 dotnet publish LenovoLegionToolkit.WPF -c release -o build /p:DebugType=None /p:FileVersion=%VERSION% /p:Version=%VERSION% || exit /b
-dotnet publish LenovoLegionToolkit.SpectrumTester -c release -o build /p:DebugType=None /p:FileVersion=%VERSION% /p:Version=%VERSION% || exit /b
-dotnet publish LenovoLegionToolkit.CLI -c release -o build /p:DebugType=None /p:FileVersion=%VERSION% /p:Version=%VERSION% || exit /b
+rem dotnet publish LenovoLegionToolkit.SpectrumTester -c release -o build /p:DebugType=None /p:FileVersion=%VERSION% /p:Version=%VERSION% || exit /b
+rem dotnet publish LenovoLegionToolkit.CLI -c release -o build /p:DebugType=None /p:FileVersion=%VERSION% /p:Version=%VERSION% || exit /b
 
-iscc make_installer.iss /DMyAppVersion=%VERSION% || exit /b
+rem iscc make_installer.iss /DMyAppVersion=%VERSION% || exit /b
+
+@for /f "delims=" %%e in ('dir /A:D /S /B build\*^') do @if exist "%%e" (@rmdir /S /Q %%e)
